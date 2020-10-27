af31ffdbd0f89e203944d92b9bc255a1e7f7758e
Jbadz <60369466+IamPain7@users.noreply.github.com>
2020-10-26 21:06:20 -0400

    MethodRecap file added

---
+++ MethodsRecap/MethodsRecap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodsRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           1:Write a C# program that takes user input and creates a story.
+            Ask the user for things like a name, color, an animal, etc. Make up a story and be creative.
+            Hint:
+            Use Console.ReadLine() to take user input and store the input into a variable.
+            For example:  
+            Ask the user “What is your name?”
+            Store the input
+
+            EXAMPLE:
+                var name = Console.ReadLine();
+
+            Record user input, for each of these. Combine all answers for a fun, short story. 
+            Hint:
+
+            Name: Michael
+            Favorite Color: Blue
+            Favorite Animal: Walrus
+            Favorite Band: The Beatles
+            Extra points - turn the answers into a fun short story!
+             */
+
+
+        }
+    }
+}
