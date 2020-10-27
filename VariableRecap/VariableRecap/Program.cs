412d4a1ee956cb981176a850e229b42e29bb0392
Jbadz <60369466+IamPain7@users.noreply.github.com>
2020-10-26 20:43:28 -0400

    VariableRecap file added

---
+++ VariableRecap/VariableRecap/Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+
+namespace VariableRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Step 1: Declare and initialize variables for the following types: 
+            string 
+            int 
+            char 
+            bool 
+            double
+            decimal 
+
+           Step 2: Concatenate these variables in a Console.Writeline();
+           For example:
+           string dogName = "Ralph";
+           int dogAge = 10;    
+           Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+
+           Step 3: Analyze any errors you might incur, if any, and research how to fix it.
+           This is a prime opportunity to exemplify self-learning.
+
+            */
+
+        }
+    }
+}
