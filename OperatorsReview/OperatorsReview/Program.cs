48a2348e28ebc24e67feeaaa287d4e271c70734e
IamPain7 <jjvirtudazo@gmail.com>
2020-10-28 20:26:25 -0600

    Added file

---
+++ OperatorsReview/OperatorsReview/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Prefix sample:
+            int a = 1;
+            int b = ++a;
+
+            Console.WriteLine($" The answer of A is {a} the answer of b is {b}");
+
+            //post fix sample 
+            int c = 2;
+            int d = c++;
+
+            Console.WriteLine($" The answe of C is {c} the answer of D is{d}");
+
+
+                }
+    }
+}
